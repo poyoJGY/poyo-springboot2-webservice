8e1dacfc8f72467a750882bd382bc80e4a8479e8
jgy <duke407@naver.com>
2025-05-17 22:37:41 +0900

    Initial commit

---
+++ src/main/java/com/jgy/book/poyo_springboot2_webservice/PoyoSpringboot2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jgy.book.poyo_springboot2_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoyoSpringboot2WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoyoSpringboot2WebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgy/book/poyo_springboot2_webservice/PoyoSpringboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgy.book.poyo_springboot2_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoyoSpringboot2WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
