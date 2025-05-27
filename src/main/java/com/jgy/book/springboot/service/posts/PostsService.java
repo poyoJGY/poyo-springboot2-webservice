72fa82d2e131447a64fec01c1c86a2e599377337
jgy <duke407@naver.com>
2025-05-27 22:17:42 +0900

    등록 기능 추가

---
+++ src/main/java/com/jgy/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.jgy.book.springboot.service.posts;
+
+import com.jgy.book.springboot.domain.posts.PostsRepository;
+import com.jgy.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
