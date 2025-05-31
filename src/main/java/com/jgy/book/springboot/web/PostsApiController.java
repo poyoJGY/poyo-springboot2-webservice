d4e5e231bac2e549fcba77b6b72f610bc3384c0c
jgy <duke407@naver.com>
2025-06-01 00:55:30 +0900

    오타 수정

--- src/main/java/com/jgy/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jgy/book/springboot/web/PostsApiController.java
@@ -28,7 +28,7 @@ public class PostsApiController {
         return postsService.findById(id);
     }
 
-    @DeleteMapping("/api/vi/posts/{id}")
+    @DeleteMapping("/api/v1/posts/{id}")
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
         return id;
