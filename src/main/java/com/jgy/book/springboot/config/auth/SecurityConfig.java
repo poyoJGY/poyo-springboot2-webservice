0adf576c532868cf6a8c9a5fa245d2772bd2bb5a
jgy <duke407@naver.com>
2025-06-06 17:23:18 +0900

    소셜 로그인 설정

---
+++ src/main/java/com/jgy/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.jgy.book.springboot.config.auth;
+
+import com.jgy.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                        .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
+                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                )
+                .build();
+    }
+}
