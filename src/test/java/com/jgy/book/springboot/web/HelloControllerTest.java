eb0ac04389ca971b9af0defbd67cecef1a9ad4c2
jgy <duke407@naver.com>
2025-06-12 23:58:30 +0900

    @WithMockUser로 임의 인증된 사용자 추가

--- src/test/java/com/jgy/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jgy/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.jgy.book.springboot.web;
 
+import com.jgy.book.springboot.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,12 +19,17 @@ import static org.hamcrest.Matchers.is;
 @SpringBootTest와 @WebMvcTest를 같이 사용하면  initializationError 발생
  둘 다 ApplicationContext를 로딩하려고 하면서 충돌 발생
 */
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -30,6 +39,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
